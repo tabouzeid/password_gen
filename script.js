4e747c3b94a8177680bf209d60ba65fc3d5bc521
Taher Abouzeid <tabouzeid@gmail.com>
2020-05-23 19:55:45 -0400

    added method to gather password details and validate them

--- script.js
+++ script.js
@@ -10,6 +10,49 @@ var specialChars = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', 
                     '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{',
                     '|','}','~'];
 
+function getPasswordDetails(){
+  return {
+    "passwordLength": parseInt(passwordLengthField.value),
+    "includeUpperCase": upperCaseCheckbox.checked,
+    "includeLowerCase": lowerCaseCheckbox.checked,
+    "includeSpecialCase": specialCaseCheckbox.checked,
+    "includeNumbers": numbersCheckbox.checked,
+    "selectedAttNum": 0,
+    "isValid": function(){
+      if (this.passwordLength < 8){
+        alert("Password cannot be less than 8 characters long!");
+        return false;
+      } else if (this.passwordLength > 128) {
+        alert("Password cannot be more than 128 characters long!");
+        return false;
+      }
+
+      if(this.includeUpperCase) {
+        this.selectedAttNum++;
+      }
+
+      if(this.includeLowerCase) {
+        this.selectedAttNum++;
+      }
+
+      if(this.includeSpecialCase) {
+        this.selectedAttNum++;
+      }
+
+      if(this.includeNumbers) {
+        this.selectedAttNum++;
+      }
+
+      if(this.selectedAttNum == 0){
+        alert("At least one checkbox must be checked!")
+        return false;
+      }
+
+      return true;
+    }
+  }
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
