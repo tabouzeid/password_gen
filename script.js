f49581b06a041f8a05841affd763455293a6feb0
Taher Abouzeid <tabouzeid@gmail.com>
2020-05-23 19:52:54 -0400

    getting references to various components

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var modalButton = document.querySelector("#modelButton");
+var passwordLengthField = document.querySelector("#passLength");
+var upperCaseCheckbox = document.querySelector("#uppercase-check");
+var lowerCaseCheckbox = document.querySelector("#lowercase-check");
+var numbersCheckbox = document.querySelector("#numbers-check");
+var specialCaseCheckbox = document.querySelector("#specchars-check");
 
 // Write password to the #password input
 function writePassword() {
